470ce0e8166e2c11c5bc9dab34d21165cb9b2886
Yuri Catramby <yuricatramby@Yuris-MacBook-Air.local>
2023-07-05 18:26:50 -0300

    add

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require("http");
 const port = 3000;
 
+const routes = {
+  "/": "Node Course",
+  "/books": "book page",
+  "/authors": "authors list",
+  "/publish_company": "publish company",
+  "/about": "Project Info",
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Node Course");
+  res.end(routes[req.url]);
 });
 
 server.listen(port, () => {
